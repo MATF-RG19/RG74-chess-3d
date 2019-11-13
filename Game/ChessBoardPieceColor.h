b87153a81aaf476603322525a10e29827d2dffb9
ruzica4 <ruzicagagic4@gmail.com>
2019-11-13 02:25:06 +0100

    declared chessboard field colors

---
+++ Game/ChessBoardPieceColor.h
@@ -0,0 +1,10 @@
+#ifndef CHESSBOARDPIECECOLOR_H
+#define CHESSBOARDPIECECOLOR_H
+
+// pieces on the board can be either black or white
+enum ChessBoardPieceColor : int {
+  BLACK,
+  WHITE
+};
+
+#endif
