c98cd314533585967dd590aa403cc96faf5fc0cf
ruzica4 <ruzicagagic4@gmail.com>
2019-11-13 02:27:56 +0100

    declared chessboard piece types

---
+++ Game/ChessBoardPieceType.h
@@ -0,0 +1,15 @@
+#ifndef CHESSBOARDPIECETYPE_H
+#define CHESSBOARDPIECETYPE_H
+
+// there are 6 figures in chess, and to each
+// of them we assign a value
+enum ChessBoardPieceType : int {
+  QUEEEN,
+  ROOK,
+  BISHOP,
+  KNIGHT,
+  KING,
+  PAWN
+};
+
+}
